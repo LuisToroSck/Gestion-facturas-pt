9cf9f6725281af2fe591db15cdb6f9f3cc16487e
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-11 18:24:10 -0400

    add invoices ruta

--- GestionFacturas-fe/gestionfacturas-fe/src/components/navbar.jsx
+++ GestionFacturas-fe/gestionfacturas-fe/src/components/navbar.jsx
@@ -1,18 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function NavbarComponent() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary px-4 fixed-top w-100 shadow-sm">
             <Container>
-                <Navbar.Brand href="#home">Invoice Manager</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Invoice Manager</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Dashboard</Nav.Link>
-                        <Nav.Link href="#link">Invoices</Nav.Link>
-                        <Nav.Link href="#link">Reports</Nav.Link>
+                        <Nav.Link as={Link} to="/">Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to="/invoices">Invoices</Nav.Link>
+                        <Nav.Link as={Link} to="/reports">Reports</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
