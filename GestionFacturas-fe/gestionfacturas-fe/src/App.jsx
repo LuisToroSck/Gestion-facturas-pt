9a652bd2767adf714cafa7d5160cc8e2d7e55c92
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-11 18:23:36 -0400

    add path invoices

--- GestionFacturas-fe/gestionfacturas-fe/src/App.jsx
+++ GestionFacturas-fe/gestionfacturas-fe/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Invoices from './pages/invoices';
+import Dashboard from './pages/dashboard';
 import NavbarComponent from './components/navbar';
+import Invoices from './pages/invoices';
 
 function App() {
   return (
     <BrowserRouter>
       <NavbarComponent />
         <Routes>
-          <Route path="/" element={<Invoices />} />
+          <Route path="/" element={<Dashboard />} />3
+          <Route path="/invoices" element={<Invoices />} />
         </Routes>
     </BrowserRouter>
   );
