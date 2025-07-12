d2f5a7d8360b94448ad3df199ebe647ab05a0e5c
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-12 02:43:07 -0400

    add react-toastify

--- GestionFacturas-fe/gestionfacturas-fe/src/App.jsx
+++ GestionFacturas-fe/gestionfacturas-fe/src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
 import NavbarComponent from './components/navbar';
 import Invoices from './pages/invoices';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
       <NavbarComponent />
+      <ToastContainer position="top-right" autoClose={3000} />
         <Routes>
           <Route path="/" element={<Dashboard />} />3
           <Route path="/invoices" element={<Invoices />} />
