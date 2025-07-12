326fd8ac16a0dedfd6515608e4ff8f10a023c88c
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-12 10:28:30 -0400

    add estado pago resumen model

---
+++ GestionFacturas-be/Models/EstadiPagoResumenDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionFacturas_be.Models
+{
+    public class EstadoPagoResumenDto
+    {
+        public string Estado { get; set; }
+        public int Total { get; set; }
+        public decimal Porcentaje { get; set; }
+        public decimal MontoAcumulado { get; set; }
+    }
+}
