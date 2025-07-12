23485b4056bd03f2836f50976664d97df00ff62e
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-12 11:06:58 -0400

    add inconsistent invoice model

---
+++ GestionFacturas-be/Models/InconsistentInvoice.cs
@@ -0,0 +1,12 @@
+﻿namespace GestionFacturas_be.Models
+{
+    public class FacturaInconsistenteDto
+    {
+        public int InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal SumSubtotals { get; set; }
+        public string InvoiceStatus { get; set; }
+    }
+}
