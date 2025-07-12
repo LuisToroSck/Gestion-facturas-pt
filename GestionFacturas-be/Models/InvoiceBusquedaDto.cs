1faa8a5b71662c62345473dfbe2ffbec2a0f32de
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-12 12:07:01 -0400

    add invoicesearch model

---
+++ GestionFacturas-be/Models/InvoiceBusquedaDto.cs
@@ -0,0 +1,14 @@
+﻿namespace GestionFacturas_be.Models
+{
+    public class InvoiceBusquedaDto
+    {
+        public int Id { get; set; }
+        public int InvoiceNumber { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime PaymentDueDate { get; set; }
+        public string InvoiceStatus { get; set; }
+        public string PaymentStatus { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
