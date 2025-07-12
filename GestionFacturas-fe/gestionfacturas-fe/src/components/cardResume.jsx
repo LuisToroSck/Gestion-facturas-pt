708830c6e6d8cd9746da059b56928768135de617
Luis Javier Toro Flores <luis.toro.f@usach.cl>
2025-07-12 10:27:13 -0400

    style color bg cards

--- GestionFacturas-fe/gestionfacturas-fe/src/components/cardResume.jsx
+++ GestionFacturas-fe/gestionfacturas-fe/src/components/cardResume.jsx
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card';
 
 function CardResume({ title, value }) {
     return (
-        <Card>
+        <Card style={{backgroundColor:"#f5f5f5", border:"none"}}>
             <Card.Body>
                 <Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
                 <Card.Text>
